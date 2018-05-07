16344a4e6aab9125f1d5b5d460ce1f5eceb59624
Azrty <38593367+Azrty@users.noreply.github.com>
2018-05-07 19:53:39 -0400

    Create database.php

---
+++ app/Config/database.php
@@ -0,0 +1,15 @@
+<?php
+class DATABASE_CONFIG {
+
+    public $default = array(
+        'datasource' => 'Database/Mysql',
+        'persistent' => false,
+        'host' => process.env.host,
+        'login' => process.env.login,
+        'password' => process.env.password,
+        'database' => process.env.database,
+        'encoding' => 'utf8',
+    );
+}
+
+
