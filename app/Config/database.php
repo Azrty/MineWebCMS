63a1438f7bd1ce61016eff759d3773f583080fda
Azrty <38593367+Azrty@users.noreply.github.com>
2018-05-07 20:41:11 -0400

    Update database.php

--- app/Config/database.php
+++ app/Config/database.php
@@ -1,13 +1,17 @@
 <?php
+$_host = getenv('host');
+$_database = getenv('database');
+$_password = getenv('password');
+$_login = getenv('login');
 class DATABASE_CONFIG {
 
     public $default = array(
         'datasource' => 'Database/Mysql',
         'persistent' => false,
-        'host' => '$process.env.host',
-        'login' => '$process.env.login',
-        'password' => '$process.env.password',
-        'database' => '$process.env.database',
+        'host' => $_host,
+        'login' => $_login,
+        'password' => $_password,
+        'database' => $_database,
         'encoding' => 'utf8'
     );
 }
