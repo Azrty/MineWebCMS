59e6f566d71312c035e63a07f184847402ad20b5
Azrty <38593367+Azrty@users.noreply.github.com>
2018-05-07 20:54:40 -0400

    Update database.php

--- app/Config/database.php
+++ app/Config/database.php
@@ -4,10 +4,10 @@ class DATABASE_CONFIG {
     public $default = array(
         'datasource' => 'Database/Mysql',
         'persistent' => false,
-        'host' => ENV["host"],
-        'login' => ENV["login"],
-        'password' => ENV["password"],
-        'database' => ENV["database"],
+        'host' => ENV["HOST"],
+        'login' => ENV["LOGIN"],
+        'password' => ENV["PASSWORD"],
+        'database' => ENV["DATABASE"],
         'encoding' => 'utf8'
     );
 }
